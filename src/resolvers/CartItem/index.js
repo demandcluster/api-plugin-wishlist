e84f5ae80061cf1627b8b0f182b7b489dc4f2d5a
gflechsig <gillywerk@gmail.com>
2021-03-22 18:07:23 +0100

    wishlist change 2

--- src/resolvers/CartItem/index.js
+++
@@ -1,9 +0,0 @@
-import resolveShopFromShopId from "@reactioncommerce/api-utils/graphql/resolveShopFromShopId.js";
-import { encodewishlistItemOpaqueId } from "../../xforms/id.js";
-import productTags from "./productTags.js";
-
-export default {
-  _id: (node) => encodewishlistItemOpaqueId(node._id),
-  productTags,
-  shop: resolveShopFromShopId
-};
